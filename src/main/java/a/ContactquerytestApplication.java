ba10ee11a055188a32b4da12e733ecf876ff6e4d
loganisha <69141688+loganisha@users.noreply.github.com>
2022-01-15 11:37:05 +0530

    Add files via upload
    contactquery using springboot

---
+++ src/main/java/a/ContactquerytestApplication.java
@@ -0,0 +1,13 @@
+package a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactquerytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactquerytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/a/ContactquerytestApplicationTests.java
@@ -0,0 +1,13 @@
+package a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactquerytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
